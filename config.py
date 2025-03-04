4cc71bab7af1db74c30987df0e7fc59c6e96426e
Hugo Oropeza <oropezahugo35@gmail.com>
2025-03-04 15:56:32 -0400

    sharding

---
+++ config.py
@@ -0,0 +1,12 @@
+DB_CONFIG_SHARD_1 = {
+    "host":"localhost",
+    "user":"root", 
+    "password":"focafeliz", 
+    "database":"shard1"
+}
+DB_CONFIG_SHARD_2 = {
+    "host":"localhost",
+    "user":"root", 
+    "password":"focafeliz", 
+    "database":"shard2"
+}
---
+++ sharding.py
@@ -0,0 +1,41 @@
+# instalar con: python3 -m pip install mysql-connector-python
+import mysql.connector
+from config import DB_CONFIG_SHARD_1
+from config import DB_CONFIG_SHARD_2
+
+shard_connections = {
+    "shard1": mysql.connector.connect(
+        **DB_CONFIG_SHARD_1
+    ),
+    "shard2": mysql.connector.connect(
+        **DB_CONFIG_SHARD_2
+    ),
+}
+
+def get_shard(user_email):
+    return "shard1" if user_email % 2 != 0 else "shard2"
+
+def insert_user(name: str, email: str):
+    shard = get_shard(len(email))
+    conn = shard_connections[shard]
+    cursor = conn.cursor()
+    
+    query = "INSERT INTO users (name, email) VALUES (%s, %s);"
+    cursor.execute(query, (name, email))
+    conn.commit()
+    print(f"Usuario {name} insertado en {shard}")
+
+def get_user(email):
+    shard = get_shard(len(email))
+    conn = shard_connections[shard]
+    cursor = conn.cursor()
+    query = "SELECT id, name, email FROM users WHERE email = %s"
+    cursor.execute(query, (email,))
+    result = cursor.fetchone()
+    return result
+
+#insert_user("Lucas", "lucas3@example.com")
+#insert_user("Paola", "paola25@example.com")
+
+print(get_user("lucas@example.com"))  
+
