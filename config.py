3d6e5b28a4581dd230ab7db17d166e9051dcba85
Hugo Oropeza <oropezahugo35@gmail.com>
2025-03-04 15:59:34 -0400

    config

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 DB_CONFIG_SHARD_1 = {
     "host":"localhost",
     "user":"root", 
-    "password":"focafeliz", 
+    "password":"contraseña", 
     "database":"shard1"
 }
 DB_CONFIG_SHARD_2 = {
     "host":"localhost",
     "user":"root", 
-    "password":"focafeliz", 
+    "password":"contraseña", 
     "database":"shard2"
 }
